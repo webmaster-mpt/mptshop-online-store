28c67fc6309c0f5271238dc76e6441ba93c7ef04
Sino <74964601+webmaster-mpt@users.noreply.github.com>
2022-08-21 16:45:26 +0300

    Change Email and Password
    Change Email and Password

--- config/main.php
+++ config/main.php
@@ -52,8 +52,8 @@ return [
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'p_s.muzafari@mpt.ru',
-                'password' => '01121963papa',
+                'username' => 'email',
+                'password' => 'password',
                 'port' => ' 465', // Port 25 is a very common port too
                 'encryption' => 'ssl', // It is often used, check your provider or mail server specs
             ],
